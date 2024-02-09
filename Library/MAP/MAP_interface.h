4b8b8dc4298c6cf4520a0fbf37fccddab80129f6
AhmedFaheem <a7medfaheem@gmail.com>
2024-02-09 18:17:13 +0200

    Adding Map Library Files

---
+++ Library/MAP/MAP_interface.h
@@ -0,0 +1,9 @@
+#ifndef MAP_INTERFACE_H
+#define MAP_INTERFACE_H
+
+
+sint32 MAP(sint32 Copy_s32InputRangeMin, sint32 Copy_s32InputRangeMax,sint32 Copy_s32OutputRangeMin,sint32 Copy_s32OutputRangeMax, sint32 Copy_s32Result );
+
+
+
+#endif 
