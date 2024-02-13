bcf2c9bdd8f9a93a71eaea39ff9d3726c9934973
Ahmed Faheem <43971128+ahmedfaheem@users.noreply.github.com>
2024-02-14 01:54:52 +0200

    Update LM35_interface.h

--- HAL/LM35/LM35_interface.h
+++ HAL/LM35/LM35_interface.h
@@ -2,7 +2,19 @@
 #define LM35_INTERFACE_H
 
 
-/* This Func work with ADC driver*/
+/* This Function work with ADC driver by Synchronous Function with this channels
+
+1-	ADC_u8SINGLE_ENDED_ADC0,
+2-	ADC_u8SINGLE_ENDED_ADC1,
+3-	ADC_u8SINGLE_ENDED_ADC2,
+4-	ADC_u8SINGLE_ENDED_ADC3,
+5-	ADC_u8SINGLE_ENDED_ADC4,
+6-	ADC_u8SINGLE_ENDED_ADC5,
+7-	ADC_u8SINGLE_ENDED_ADC6,
+8-	ADC_u8SINGLE_ENDED_ADC7,
+
+*/
+
 uint8 LM34_u8GetTempInC(uint8 Copy_u8ADCChannel);
 
 
