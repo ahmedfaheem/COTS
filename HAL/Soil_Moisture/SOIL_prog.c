bbee68da9d6237aa56994f2399268a7c4906f638
AhmedFaheem <a7medfaheem@gmail.com>
2024-07-04 00:20:11 +0300

    edit on 8 bit mode

--- HAL/Soil_Moisture/SOIL_prog.c
+++ HAL/Soil_Moisture/SOIL_prog.c
@@ -11,7 +11,7 @@ uint16 SOIL_u16GetHumidity(uint16 Copy_u16ADCVal){
 	
 #if  SOIL_ADC_MODE == SOIL_8BIT_MODE
 
-Local_u16Reading  = 100UL - ((Copy_u16ADCVal * 100UL)/255UL);
+Local_u16Reading  = ((Copy_u16ADCVal * 100UL)/255UL);
 
 
 #elif SOIL_ADC_MODE  == SOIL_10BIT_MODE
